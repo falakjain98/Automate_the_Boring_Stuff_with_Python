8be484dfa3f423dc9cb650cd151fc22c5767c6d8
falakjain <jain.falak@hotmail.com>
2021-01-27 09:37:00 +0530

    Correction to python code

--- Working with PDF and Word Documents/combinePdfs.py
+++ Working with PDF and Word Documents/combinePdfs.py
@@ -5,20 +5,19 @@ import PyPDF2, os
 
 # Get all PDF filenames
 pdfFiles = []
-for filename in os.listdir('.'):
+for filename in os.listdir('./minutes'):
     if filename.endswith('.pdf'):
         pdfFiles.append(filename)
 pdfFiles.sort(key = str.lower)
 pdfWriter = PyPDF2.PdfFileWriter()
-
-path = os.getcwd()
 # Loop through all the PDF files
 for filename in pdfFiles:
-    pdfFileObj = open(str(filename),'rb')
+    pdfFileObj = open(filename,'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
     # Loop through all the pages (except the first) and add them
     for pageNum in range(1,pdfReader.numPages):
         pageObj = pdfReader.getPage(pageNum)
+        pdfWriter.addPage(pageObj)
 
 # Save the resulting PDF to a file
 pdfOutput = open('allminutes.pdf','wb')
